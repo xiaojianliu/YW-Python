fd2de3210c167ab3e3a45d6c0bf63a7968315bf2
“jamespatrickmanning“ <james.manning@noaa.gov>
2013-02-22 15:12:49 -0500

    compare mod and obs data

--- modvsobs.py
+++ modvsobs.py
@@ -12,9 +12,14 @@ from utilities import nearxy,my_x_axis_format
 import pandas as pd
 from numpy import float64
 from datetime import datetime, timedelta
+from models import get_mod_bottom_temp
 
-#########creat obs da##############
+
 def resamda(oritso):
+        '''
+        resample daily average data
+        add some new columns like 'count mean,median,max,min,std,yy,mm,dd'
+        '''     
         resamda=float64(oritso[0]).resample('D',how=['count','mean','median','min','max','std'])
         resamda.ix[resamda['count']<minnumperday,['mean','median','min','max','std']] = 'NaN'
         resamda['yy']=resamda.index.year
@@ -22,9 +27,12 @@ def resamda(oritso):
         resamda['dd']=resamda.index.day
         output_fmt=['yy','mm','dd','count','mean','median','min','max','std']
         resamdaf=resamda.reindex(columns=output_fmt)  
-        return resamda,resamdaf
-#########creat obs ma##############
+        return resamdaf
+
 def resamma(oritso):
+        '''
+        resample month average data
+        '''
         resamma=float64(oritso[0]).resample('m',how=['count','mean','median','min','max','std'],kind='period')
         resamma.ix[resamma['count']<25*numperday,['mean','median','min','max','std']] = 'NaN'
         resamma['yy']=resamma.index.year
@@ -32,9 +40,12 @@ def resamma(oritso):
         resamma['dd']=15
         output_fmt=['yy','mm','dd','count','mean','median','min','max','std']
         resammaf=resamma.reindex(columns=output_fmt)# found I needed to generate a new dataframe to print in this order
-        return resamma,resammaf
-#########creat obs dc###############
+        return resammaf
+
 def resamdc(resamda):
+        '''
+        resample daily climatology
+        '''
         newindex=[]
         for j in range(len(resamda)):    
                 newindex.append(resamda['mean'].index[j].replace(year=2000)) # puts all observations in the same year
@@ -45,9 +56,12 @@ def resamdc(resamda):
         resamdc['dd']=resamdc.index.day
         output_fmt=['yy','mm','dd','count','mean','median','min','max','std']
         resamdcf=resamdc.reindex(columns=output_fmt)# found I needed to generate a new dataframe to print in this order
-        return resamdc,resamdcf
-############creat obs mc###################
+        return resamdcf
+
 def resammc(resamdc):
+        '''
+        resample month climatology
+        '''
         resammc=resamdc['mean'].resample('m',how=['mean','median'],loffset=timedelta(days=-15))
         resammc['count']=0
         resammc['min']=0.
@@ -67,8 +81,8 @@ def resammc(resamdc):
         resammc['dd']=0
         output_fmt=['yy','mm','dd','count','mean','median','min','max','std']
         resammcf=resammc.reindex(columns=output_fmt)# found I needed to generate a new dataframe to print in this order
-        return resammc,resammcf
-#########caculate dadc diff####################
+        return resammcf
+
 def diffdadc(diff,output_fmt):
         day=[]
         for i in range(len(diff)):
@@ -78,33 +92,7 @@ def diffdadc(diff,output_fmt):
         difff=diff.reindex(columns=output_fmt)
         difff.columns=['date','mean','median','min','max','std']
         return difff
-
-
-site=['AG01','BA01']
-minnumperday=18
-numperday=24
-for k in range(len(site)):
-#################read-in obs data##################################
-        [lati,loni,on]=getemolt_latlon(site,k) # extracts lat/lon based on site code
-        [lati,loni]=dm2dd(lati,loni) #converts decimal-minutes to decimal degrees
-        [obs_dt,obs_temp]=getemolt_temp(site,k) # extracts time series
-        obs_dtindex=[]
-        for kk in range(len(obs_temp)):
-            obs_temp[kk]=f2c(obs_temp[kk]) # converts to Celcius
-            obs_dtindex.append(datetime.strptime(str(obs_dt[kk])[:10],'%Y-%m-%d'))
-        obstso=pd.DataFrame(obs_temp,index=obs_dtindex)
-
-##################################################################
-        [reobsda,reobsdaf]=resamda(obstso) 
-        reobsdaf.to_csv(site[k]+'_wtmp_da_obs.csv',index=False,header=False,na_rep='NaN',float_format='%10.2f')   
-        [reobsma,reobsmaf]=resamma(obstso)
-        reobsmaf.to_csv(site[k]+'_wtmp_ma_obs.csv',index=False,header=False,na_rep='NaN',float_format='%10.2f')  
-        [reobsdc,reobsdcf]=resamdc(reobsda)        
-        reobsdcf.to_csv(site[k]+'_wtmp_dc_obs.csv',index=False,header=False,na_rep='NaN',float_format='%10.2f')
-        [obsmc,obsmcf]=resammc(reobsdc)
-        obsmcf.to_csv(site[k]+'_wtmp_mc_obs.csv',index=False,header=False,na_rep='NaN',float_format='%10.2f')
-###################read-in mod data#################################
-
+def get_mod_bottom_temp(lati,loni,starttime,endtime,depth):
         urlfvcom = 'http://www.smast.umassd.edu:8080/thredds/dodsC/fvcom/hindcasts/30yr_gom3'
         nc = netCDF4.Dataset(urlfvcom)
         nc.variables
@@ -116,43 +104,73 @@ for k in range(len(site)):
         var = nc.variables[vname]
 
         inode = nearxy(lon,lat,loni,lati)
-        modindex=netCDF4.date2index([obs_dt[0],obs_dt[-1]],times,select='nearest')
-        modtso=pd.DataFrame(var[modindex[0]:modindex[1],44,inode],index=jd[modindex[0]:modindex[1]])
-#####################################################
-        [remodda,remoddaf]=resamda(modtso) 
-        remoddaf.to_csv(site[k]+'_wtmp_da_obs.csv',index=False,header=False,na_rep='NaN',float_format='%10.2f')   
-        [remodma,remodmaf]=resamma(modtso)
-        remodmaf.to_csv(site[k]+'_wtmp_ma_obs.csv',index=False,header=False,na_rep='NaN',float_format='%10.2f')  
-        [remoddc,remoddcf]=resamdc(remodda)        
-        remoddcf.to_csv(site[k]+'_wtmp_dc_obs.csv',index=False,header=False,na_rep='NaN',float_format='%10.2f')
-        [modmc,modmcf]=resammc(remoddc)
-        modmcf.to_csv(site[k]+'_wtmp_mc_obs.csv',index=False,header=False,na_rep='NaN',float_format='%10.2f')
-        
+        modindex=netCDF4.date2index([starttime,endtime],times,select='nearest')
+        modtso=pd.DataFrame(var[modindex[0]:modindex[1],depth,inode],index=jd[modindex[0]:modindex[1]])    
+        return modtso,var,inode
+
+site=['AG01','BA01']
+minnumperday=18
+numperday=24
+for k in range(len(site)):
+#################read-in obs data##################################
+        [lati,loni,on]=getemolt_latlon(site[k]) # extracts lat/lon based on site code
+        [lati,loni]=dm2dd(lati,loni) #converts decimal-minutes to decimal degrees
+        [obs_dt,obs_temp]=getemolt_temp(site[k]) # extracts time series
+        obs_dtindex=[]
+        for kk in range(len(obs_temp)):
+            obs_temp[kk]=f2c(obs_temp[kk]) # converts to Celcius
+            obs_dtindex.append(datetime.strptime(str(obs_dt[kk])[:10],'%Y-%m-%d'))
+        obstso=pd.DataFrame(obs_temp,index=obs_dtindex)
+
+##################generate resample DataFrame and putput file################################################
+        reobsdaf=resamda(obstso) 
+        reobsdaf.to_csv(site[k]+'_wtmp_da_obs.csv',index=False,header=False,na_rep='NaN',float_format='%10.2f')   
+        reobsmaf=resamma(obstso)
+        reobsmaf.to_csv(site[k]+'_wtmp_ma_obs.csv',index=False,header=False,na_rep='NaN',float_format='%10.2f')  
+        reobsdcf=resamdc(reobsdaf)        
+        reobsdcf.to_csv(site[k]+'_wtmp_dc_obs.csv',index=False,header=False,na_rep='NaN',float_format='%10.2f')
+        reobsmcf=resammc(reobsdcf)
+        reobsmcf.to_csv(site[k]+'_wtmp_mc_obs.csv',index=False,header=False,na_rep='NaN',float_format='%10.2f')
+###################read-in mod data#################################
+
+        starttime=obs_dt[0]
+        endtime=obs_dt[-1]
+        [modtso,var,inode]=get_mod_bottom_temp(lati,loni,starttime,endtime,depth=44)
+##############generate resample DataFrame and putput file#############
+        remoddaf=resamda(modtso) 
+        remoddaf.to_csv(site[k]+'_wtmp_da_mod.csv',index=False,header=False,na_rep='NaN',float_format='%10.2f')   
+        remodmaf=resamma(modtso)
+        remodmaf.to_csv(site[k]+'_wtmp_ma_mod.csv',index=False,header=False,na_rep='NaN',float_format='%10.2f')  
+        remoddcf=resamdc(remoddaf)        
+        remoddcf.to_csv(site[k]+'_wtmp_dc_mod.csv',index=False,header=False,na_rep='NaN',float_format='%10.2f')
+        remodmcf=resammc(remoddcf)
+        remodmcf.to_csv(site[k]+'_wtmp_mc_mod.csv',index=False,header=False,na_rep='NaN',float_format='%10.2f')
 ##############plot da compare figure##################
         fig=plt.figure(figsize=(16,10))
         ax=fig.add_subplot(211)
         ax.plot_date(obs_dt,obs_temp,fmt='-')
         plt.grid()
-        ax.plot_date(jd[modindex[0]:modindex[1]],var[modindex[0]:modindex[1],44,inode],fmt='-',color='red')#bottom most value equals 44
+        ax.plot_date(modtso.index,modtso.values,fmt='-',color='red')#bottom most value equals 44
         plt.ylabel(var.units)
-        plt.title('eMOLT site '+site[k]+' temp vs FVCOM '+'%s at node=%d (Lon=%.4f, Lat=%.4f)' % (vname, inode+1, lon[inode], lat[inode]))
+        plt.title('eMOLT site '+site[k]+' temp vs FVCOM '+'%s at node=%d' % ('temp', inode+1))
         plt.legend(['observed','modeled'],loc='best')
-        plt.show()
+#        plt.show()
 ###############plot mc compare figure##################
-        TimeDelta=obsmcf.index[-1]-obsmcf.index[0]          
+        TimeDelta=reobsmcf.index[-1]-reobsmcf.index[0]          
         ax1 = fig.add_subplot(212)
         my_x_axis_format(ax1, TimeDelta)
-        ax1.plot_date(obsmcf.index,obsmcf['mean'],fmt='-')
+        ax1.plot_date(reobsmcf.index,reobsmcf['mean'],fmt='-')
         plt.grid()
-        ax1.plot_date(modmcf.index,modmcf['mean'],fmt='-',color='red')#bottom most value equals 44
+        ax1.plot_date(remodmcf.index,remodmcf['mean'],fmt='-',color='red')#bottom most value equals 44
         plt.ylabel(var.units)
-        plt.title('eMOLT site '+site[k]+' temp vs FVCOM '+'%s at node=%d (Lon=%.4f, Lat=%.4f)' % (vname, inode+1, lon[inode], lat[inode]))
+        plt.title('eMOLT site '+site[k]+' temp vs FVCOM '+'%s at node=%d' % ('temp', inode+1))
+#        plt.title('eMOLT site '+site[k]+' temp vs FVCOM '+'%s at node=%d (Lon=%.4f, Lat=%.4f)' % ('temp', inode+1, lon[inode], lat[inode]))
         plt.legend(['observed','modeled'],loc='best')
         plt.show()
         plt.savefig(site[k]+'da_mc_mod_obs.png')
 ############calculate the different#######################
         output_fmt=[0,'mean','median','min','max','std']
-        diffmc=obsmcf-modmcf
+        diffmc=reobsmcf-remodmcf
         month=pd.DataFrame(range(1,13),index=diffmc.index)
         diffmc=diffmc.join(month)
         diffmcf=diffmc.reindex(columns=output_fmt)
